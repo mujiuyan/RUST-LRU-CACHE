b7be287a39655ac009e4531e6279abb46d2ac77f
xie xulin <mujiuyan@gmail.com>
2023-01-08 15:56:25 +0800

    bug fixed

--- src/lib2.rs
+++ src/lib2.rs
@@ -7,27 +7,27 @@ struct Node<K, V> {
     next: Option<NonNull<Node<K, V>>>,
 }
 
-struct KeyRef<K, V>(NonNull<Node<K, V>>);
+struct MyKey<K, V>(NonNull<Node<K, V>>);
 
-impl<K: Hash + Eq, V> Borrow<K> for KeyRef<K, V> {
+impl<K: Hash + Eq, V> Borrow<K> for MyKey<K, V> {
     fn borrow(&self) -> &K {
         unsafe { &self.0.as_ref().k }
     }
 }
 
-impl<K: Hash, V> Hash for KeyRef<K, V> {
+impl<K: Hash, V> Hash for MyKey<K, V> {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
         unsafe { self.0.as_ref().k.hash(state) }
     }
 }
 
-impl<K: Eq, V> PartialEq for KeyRef<K, V> {
+impl<K: Eq, V> PartialEq for MyKey<K, V> {
     fn eq(&self, other: &Self) -> bool {
         unsafe { self.0.as_ref().k.eq(&other.0.as_ref().k) }
     }
 }
 
-impl<K: Eq, V> Eq for KeyRef<K, V> {}
+impl<K: Eq, V> Eq for MyKey<K, V> {}
 
 impl<K, V> Node<K, V> {
     fn new(k: K, v: V) -> Self {
@@ -43,137 +43,137 @@ impl<K, V> Node<K, V> {
 pub struct LruCache<K, V> {
     head: Option<NonNull<Node<K, V>>>,
     tail: Option<NonNull<Node<K, V>>>,
-    map: HashMap<KeyRef<K, V>, NonNull<Node<K, V>>>,
-    cap: usize,
+    map: HashMap<MyKey<K, V>, NonNull<Node<K, V>>>,
+    capacity: usize,
     count_head: Option<NonNull<Node<K, usize>>>,
     count_tail: Option<NonNull<Node<K, usize>>>,
-    count_map: HashMap<KeyRef<K, usize>, NonNull<Node<K, usize>>>,
-    cap2: usize,
+    count_map: HashMap<MyKey<K, usize>, NonNull<Node<K, usize>>>,
+    capacity2: usize,
     k: usize,
     marker: PhantomData<Node<K, V>>,
 }
 
 impl<K: Hash + Eq + PartialEq, V> LruCache<K, V> {
-    pub fn new(cap: usize, cap2: usize, k: usize) -> Self {
+    pub fn new(capacity: usize, capacity2: usize, k: usize) -> Self {
         Self {
             head: None,
             tail: None,
             map: HashMap::new(),
-            cap,
+            capacity,
             count_head: None,
             count_tail: None,
             count_map: HashMap::new(),
-            cap2,
+            capacity2,
             k,
             marker: PhantomData,
         }
     }
 
     pub fn put(&mut self, k: K, v: V) -> Option<V> {
-        let node = Box::leak(Box::new(Node::new(k, v))).into();
-        let old_node = self.map.remove(&KeyRef(node)).map(|node| {
-            self.detach(node);
-            node
+        let list_node = Box::leak(Box::new(Node::new(k, v))).into();
+        let old_list_node = self.map.remove(&MyKey(list_node)).map(|list_node| {
+            self.erase(list_node);
+            list_node
         });
-        if old_node.is_none() && self.map.len() >= self.cap {
+        if old_list_node.is_none() && self.map.len() >= self.capacity {
             let tail = self.tail.unwrap();
-            self.detach(tail);
-            self.map.remove(&KeyRef(tail));
+            self.erase(tail);
+            self.map.remove(&MyKey(tail));
         }
-        self.attach(node);
-        self.map.insert(KeyRef(node), node);
-        old_node.map(|node| unsafe {
-            let node = Box::from_raw(node.as_ptr());
-            node.v
+        self.insert(list_node);
+        self.map.insert(MyKey(list_node), list_node);
+        old_list_node.map(|list_node| unsafe {
+            let list_node = Box::from_raw(list_node.as_ptr());
+            list_node.v
         })
     }
 
-    pub fn put2(&mut self, k: K, v: usize) -> Option<usize> {
-        let node = Box::leak(Box::new(Node::new(k, v))).into();
-        let old_node = self.count_map.remove(&KeyRef(node)).map(|node| {
-            self.detach2(node);
-            node
+    fn put2(&mut self, k: K, v: usize) -> Option<usize> {
+        let list_node = Box::leak(Box::new(Node::new(k, v))).into();
+        let old_list_node = self.count_map.remove(&MyKey(list_node)).map(|list_node| {
+            self.erase2(list_node);
+            list_node
         });
-        if old_node.is_none() && self.count_map.len() >= self.cap2 {
+        if old_list_node.is_none() && self.count_map.len() >= self.capacity2 {
             let tail = self.tail.unwrap();
-            self.detach2(tail);
-            self.count_map.remove(&KeyRef(tail));
+            self.erase2(tail);
+            self.count_map.remove(&MyKey(tail));
         }
-        self.attach2(node);
-        self.count_map.insert(KeyRef(node), node);
-        old_node.map(|node| unsafe {
-            let node = Box::from_raw(node.as_ptr());
-            node.v
+        self.insert2(list_node);
+        self.count_map.insert(MyKey(list_node), list_node);
+        old_list_node.map(|list_node| unsafe {
+            let list_node = Box::from_raw(list_node.as_ptr());
+            list_node.v
         })
     }
 
     pub fn get(&mut self, k: &K) -> Option<&V> {
         let count = 1;
-        if let Some(node) = self.count_map.get(k) {
-            let node = *node;
-            self.detach2(node);
-            self.attach2(node);
+        if let Some(list_node) = self.count_map.get(k) {
+            let list_node = *list_node;
+            self.erase2(list_node);
+            self.insert2(list_node);
             unsafe {
-                count = Some(&node.as_ref().v) + 1;
+                count = Some(&list_node.as_ref().v) + 1;
                 self.put2(k, count);
             }
         } else {
             self.put2(k, count);
         }
 
-        if let Some(node) = self.map.get(k) {
-            let node = *node;
-            if (count > self.k) {
-                self.detach(node);
-                self.attach(node);
+        if let Some(list_node) = self.map.get(k) {
+            let list_node = *list_node;
+            if (count >= self.k) {
+                self.erase(list_node);
+                self.insert(list_node);
             }
-            unsafe { Some(&node.as_ref().v) }
+            unsafe { Some(&list_node.as_ref().v) }
         } else {
             None
         }
     }
 
-    fn detach(&mut self, mut node: NonNull<Node<K, V>>) {
+    fn erase(&mut self, mut list_node: NonNull<Node<K, V>>) {
         unsafe {
-            match node.as_mut().prev {
+            match list_node.as_mut().prev {
                 Some(mut prev) => {
-                    prev.as_mut().next = node.as_ref().next;
+                    prev.as_mut().next = list_node.as_ref().next;
                 }
                 None => {
-                    self.head = node.as_ref().next;
+                    self.head = list_node.as_ref().next;
                 }
             }
-            match node.as_mut().next {
+            match list_node.as_mut().next {
                 Some(mut next) => {
-                    next.as_mut().prev = node.as_ref().prev;
+                    next.as_mut().prev = list_node.as_ref().prev;
                 }
                 None => {
-                    self.tail = node.as_ref().prev;
+                    self.tail = list_node.as_ref().prev;
                 }
             }
 
-            node.as_mut().prev = None;
-            node.as_mut().next = None;
+            list_node.as_mut().prev = None;
+            list_node.as_mut().next = None;
         }
     }
 
-    fn attach(&mut self, mut node: NonNull<Node<K, V>>) {
+    fn insert(&mut self, mut list_node: NonNull<Node<K, V>>) {
         match self.head {
             Some(mut head) => {
                 unsafe {
-                    head.as_mut().prev = Some(node);
-                    node.as_mut().next = Some(head);
-                    node.as_mut().prev = None;
+                    head.as_mut().prev = Some(list_node);
+                    list_node.as_mut().next = Some(head);
+                    list_node.as_mut().prev = None;
                 }
-                self.head = Some(node);
+                self.head = Some(list_node);
             }
             None => {
                 unsafe {
-                    node.as_mut().prev = None;
-                    node.as_mut().next = None;
+                    list_node.as_mut().prev = None;
+                    list_node.as_mut().next = None;
                 }
-                self.head = Some(node);
-                self.tail = Some(node);
+                self.head = Some(list_node);
+                self.tail = Some(list_node);
             }
         }
     }
@@ -181,10 +181,10 @@ impl<K: Hash + Eq + PartialEq, V> LruCache<K, V> {
 
 impl<K, V> Drop for LruCache<K, V> {
     fn drop(&mut self) {
-        while let Some(node) = self.head.take() {
+        while let Some(list_node) = self.head.take() {
             unsafe {
-                self.head = node.as_ref().next;
-                drop(node.as_ptr());
+                self.head = list_node.as_ref().next;
+                drop(list_node.as_ptr());
             }
         }
     }
